b36967ab59bacd574b9d284d126326c377e5512c
aybike <tonn.raus09@gmail.com>
2020-12-01 08:52:59 +0100

    ADDED: Structure for Checkout page | Typescript: ok

--- src/containers/Checkout.tsx
+++ src/containers/Checkout.tsx
@@ -1,7 +1,24 @@
 import React from 'react';
 
 const Checkout = (): JSX.Element => {
-  return <h1>This is Checkout</h1>;
+  return (
+    <>
+      <div className="Checkout-content">
+        Items list:
+        <div className="Checkout-item">
+          <div className="Checkout-element">
+            <h4>Item name</h4>
+            <span>10$</span>
+          </div>
+          <button type="button">Remove</button>
+        </div>
+      </div>
+      <div className="Checkout-sidebar">
+        <h3>Total price: 10$</h3>
+        <button type="button">Continue</button>
+      </div>
+    </>
+  );
 };
 
 export default Checkout;
