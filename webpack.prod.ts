b6e7d4fb1a83e3681523289315a81ef8f0d064c9
aybike <tonn.raus09@gmail.com>
2020-12-08 09:29:43 +0100

    Setting Firebase 7th  Webpack ts config splited | Github secrets & DefinePlugin |

---
+++ webpack.prod.ts
@@ -0,0 +1,66 @@
+import path from 'path';
+import webpack from 'webpack';
+import ForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import CopyPlugin from 'copy-webpack-plugin';
+
+const config: webpack.Configuration = {
+  entry: './src/index.tsx',
+  module: {
+    rules: [
+      {
+        test: /\.(ts|js)x?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react', '@babel/preset-typescript'],
+          },
+        },
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader',
+          },
+        ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/',
+  },
+  plugins: [
+    new ForkTsCheckerWebpackPlugin({
+      async: false,
+      eslint: {
+        files: './src/**/*',
+      },
+    }),
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+      filename: './index.html',
+    }),
+    new webpack.DefinePlugin({
+      'process.env': {
+        CHROMMERCE_CLIENT_ID: JSON.stringify(process.env.CHROMMERCE_CLIENT_ID),
+        CHROMMERCE_MAPS_TOKEN: JSON.stringify(process.env.CHROMMERCE_MAPS_TOKEN),
+      },
+    }),
+    new CopyPlugin({
+      patterns: [
+        { from: 'public/manifest.json', to: '' },
+        { from: 'public/service-worker.ts', to: '' },
+        { from: 'public/logochr.png', to: 'assets' },
+      ],
+    }),
+  ],
+};
+
+export default config;
